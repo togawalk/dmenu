57359628f37db029429203a2e628c6cfdf05343d
Evgenii <pitohuip@yahoo.com>
2022-08-07 01:36:26 +0500

    solarized theme for dmenu & change font

--- config.def.h
+++ config.def.h
@@ -4,14 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Jetbrains Mono:pixelsize=18:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#839496", "#002b36" },
+	[SchemeSel] = { "#eee8d5", "#073642"},
 	[SchemeOut] = { "#000000", "#00ffff" },
+
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
